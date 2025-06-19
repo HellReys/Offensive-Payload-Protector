ad759cce857f6be6ebc7e77e4f208f0b32c615ed
HellReys <wallpapertopia@gmail.com>
2025-06-19 22:05:40 +0300

    Changing branch test to main

--- setup.py
+++ setup.py
@@ -1,24 +1,74 @@
 from setuptools import setup, find_packages
+import os
+
+# Read version from version.py
+version_file = os.path.join(os.path.dirname(__file__), 'payloadprotector', 'version.py')
+version_dict = {}
+with open(version_file) as f:
+    exec(f.read(), version_dict)
+
+# Read README for long description
+readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+try:
+    with open(readme_file, 'r', encoding='utf-8') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = "Hybrid encryption tool for Red Team operations"
 
 setup(
     name="payloadprotector",
-    version="1.0.0",
-    author="HellReys",
-    description="Offensive Payload Protector",
+    version=version_dict['__version__'],
+    author=version_dict['__author__'],
+    author_email="contact.berkalicakir@gmail.com",
+    description=version_dict['__description__'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=version_dict['REPO_URL'],
     packages=find_packages(),
     install_requires=[
         'pycryptodome>=3.18.0',
-        'argparse>=1.4.0'
+        'argparse>=1.4.0',
+        'requests>=2.25.0',
+        'packaging>=21.0',
+        'setuptools>=65.0.0',
     ],
+    extras_require={
+        'dev': [
+            'pytest>=6.0',
+            'black>=21.0',
+            'flake8>=3.8',
+        ]
+    },
     entry_points={
         'console_scripts': [
             'payloadprotector=payloadprotector.cli:main',
+            'pp-update=payloadprotector.updater:main',
         ],
     },
     python_requires='>=3.6',
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Information Technology",
+        "Topic :: Security",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords="security encryption payload redteam pentest",
+    project_urls={
+        "Bug Reports": f"{version_dict['REPO_URL']}/issues",
+        "Source": version_dict['REPO_URL'],
+        "Documentation": f"{version_dict['REPO_URL']}/wiki",
+        "Fundings": "https://buymeacoffee.com/hellreys\n"
+                   "https://github.com/sponsors/HellReys",
+
+    },
 )
